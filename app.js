80111445ecab583e73aa5b3a399fd535530c07ff
Jossdel <168942707+Jossdel@users.noreply.github.com>
2025-03-05 22:04:40 -0400

    Añadi una variable

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("<h1>inicializando mini servidor en express</h1>");
 });
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server is running on port PORT");
 });
